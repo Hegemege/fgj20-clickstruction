43101b40bdad7c63bc264e8ae835c98cee30eb13
Henri Niva <henri.a.niva@gmail.com>
2020-01-31 23:37:49 +0200

    Setup new Unity input system

---
+++ src/Assets/KBMControls.cs
@@ -0,0 +1,315 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/KBMControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @KBMControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @KBMControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""KBMControls"",
+    ""maps"": [
+        {
+            ""name"": ""Default"",
+            ""id"": ""866e485d-7c45-41da-8be7-efaf1e3dc377"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Button"",
+                    ""id"": ""862c26c0-f7bc-4649-8895-5b2c64302366"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Repair"",
+                    ""type"": ""Button"",
+                    ""id"": ""50f1e950-1fff-4832-91ba-dfc2b0243c5c"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Click"",
+                    ""type"": ""Button"",
+                    ""id"": ""278a0484-a258-47da-91c8-31c3d87600b4"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector Arrow Keys"",
+                    ""id"": ""8b785b1e-212f-41f1-9fd9-04039a4a7ab3"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""f274b851-ba24-428d-8b4d-e345889c1de1"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""17c0aee2-24c7-42a6-8a25-6e9b81f787ab"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""ae21c20c-0faa-4fec-8b3b-dc70ec145516"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""b57f6992-f497-40d9-a61c-03a81b13cda5"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""2D Vector WASD"",
+                    ""id"": ""4726999a-298d-4f82-ad7d-5022f1a675dc"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""7b1ae459-062c-4619-83de-97af1136e6c1"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""2feb01f1-cf7e-49a1-bc61-48523e5da0b6"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""df09c108-d3d3-4ede-8460-0fcf0689d1ac"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""3f43966e-0eee-495e-92df-7a90cea5dd25"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""13570ea4-4a00-432b-ab0e-93d6c5279f5a"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Repair"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8d4c4c20-c730-4018-b082-7d1b98d25b82"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""DefaultScheme"",
+                    ""action"": ""Click"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""DefaultScheme"",
+            ""bindingGroup"": ""DefaultScheme"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Default
+        m_Default = asset.FindActionMap("Default", throwIfNotFound: true);
+        m_Default_Movement = m_Default.FindAction("Movement", throwIfNotFound: true);
+        m_Default_Repair = m_Default.FindAction("Repair", throwIfNotFound: true);
+        m_Default_Click = m_Default.FindAction("Click", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Default
+    private readonly InputActionMap m_Default;
+    private IDefaultActions m_DefaultActionsCallbackInterface;
+    private readonly InputAction m_Default_Movement;
+    private readonly InputAction m_Default_Repair;
+    private readonly InputAction m_Default_Click;
+    public struct DefaultActions
+    {
+        private @KBMControls m_Wrapper;
+        public DefaultActions(@KBMControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Default_Movement;
+        public InputAction @Repair => m_Wrapper.m_Default_Repair;
+        public InputAction @Click => m_Wrapper.m_Default_Click;
+        public InputActionMap Get() { return m_Wrapper.m_Default; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(DefaultActions set) { return set.Get(); }
+        public void SetCallbacks(IDefaultActions instance)
+        {
+            if (m_Wrapper.m_DefaultActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMovement;
+                @Repair.started -= m_Wrapper.m_DefaultActionsCallbackInterface.OnRepair;
+                @Repair.performed -= m_Wrapper.m_DefaultActionsCallbackInterface.OnRepair;
+                @Repair.canceled -= m_Wrapper.m_DefaultActionsCallbackInterface.OnRepair;
+                @Click.started -= m_Wrapper.m_DefaultActionsCallbackInterface.OnClick;
+                @Click.performed -= m_Wrapper.m_DefaultActionsCallbackInterface.OnClick;
+                @Click.canceled -= m_Wrapper.m_DefaultActionsCallbackInterface.OnClick;
+            }
+            m_Wrapper.m_DefaultActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Repair.started += instance.OnRepair;
+                @Repair.performed += instance.OnRepair;
+                @Repair.canceled += instance.OnRepair;
+                @Click.started += instance.OnClick;
+                @Click.performed += instance.OnClick;
+                @Click.canceled += instance.OnClick;
+            }
+        }
+    }
+    public DefaultActions @Default => new DefaultActions(this);
+    private int m_DefaultSchemeSchemeIndex = -1;
+    public InputControlScheme DefaultSchemeScheme
+    {
+        get
+        {
+            if (m_DefaultSchemeSchemeIndex == -1) m_DefaultSchemeSchemeIndex = asset.FindControlSchemeIndex("DefaultScheme");
+            return asset.controlSchemes[m_DefaultSchemeSchemeIndex];
+        }
+    }
+    public interface IDefaultActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnRepair(InputAction.CallbackContext context);
+        void OnClick(InputAction.CallbackContext context);
+    }
+}
--- src/Assets/Scripts/FixerController.cs
+++ src/Assets/Scripts/FixerController.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FixerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _inputDirection;
+
+    [SerializeField]
+    private float _baseMovementSpeed;
+
+    public float MovementSpeed
     {
-        
+        get
+        {
+            return _baseMovementSpeed;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        var dt = Time.fixedDeltaTime;
+
+        var velocity = new Vector3(_inputDirection.x, 0f, _inputDirection.y) * MovementSpeed;
+
+        transform.Translate(velocity * dt);
+    }
+
+    public void OnMovement(InputAction.CallbackContext ctx)
+    {
+        _inputDirection = ctx.ReadValue<Vector2>();
+        Debug.Log(_inputDirection);
+    }
+
+    public void OnRepair(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("OnRepair");
     }
 }
