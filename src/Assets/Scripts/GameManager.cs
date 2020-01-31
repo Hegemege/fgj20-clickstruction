6e7d6d83f9793bea79ebbb3cc1d49bc43d365f8c
Henri Niva <henri.a.niva@gmail.com>
2020-01-31 22:25:46 +0200

    Add ManagerLoader, add URP asset and default diffuse material

---
+++ src/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityUtilities;
+
+public class GameManager : GenericManager<GameManager>, ILoadedManager
+{
+    public void Initialize()
+    {
+        if (!InitializeSingleton(this)) return;
+    }
+
+    public void PostInitialize() { }
+}
