afa65146161c874c9bd12ec5db8b7046d91ce18a
Henri Niva <henri.a.niva@gmail.com>
2020-02-01 20:49:20 +0200

    Mana/coinbar interface

--- src/Assets/Scripts/ManaBar.cs
+++ src/Assets/Scripts/ManaBar.cs
@@ -5,12 +5,60 @@ using UnityEngine.UI;
 
 public class ManaBar : MonoBehaviour
 {
+    public Image BarImage;
 
-    private Image barImage;
+    private float _startValue;
+    private float _currentValue;
+    private float _targetValue;
+    private float _maxValue;
+    public float AnimationLength = 0.5f;
+    public AnimationCurve AnimationCurve;
+
+    private float _animationTimer;
+    private Coroutine _animation;
+
     private void Awake()
     {
-        barImage = transform.Find("bar").GetComponent<Image>();
+        SetAmount(30f, 100f);
+    }
 
-        barImage.fillAmount = .3f;
+    public void SetAmount(float value, float max)
+    {
+        if (_animation != null)
+        {
+            StopCoroutine(_animation);
+        }
+
+        _startValue = _currentValue;
+        _targetValue = value;
+        _maxValue = max;
+
+        _animationTimer = 0f;
+
+        _animation = StartCoroutine(AnimateBar());
+    }
+
+    private IEnumerator AnimateBar()
+    {
+        while (_animationTimer < AnimationLength)
+        {
+            _animationTimer += Time.deltaTime;
+
+            var t = Mathf.Clamp01(_animationTimer / AnimationLength);
+            var animationT = AnimationCurve.Evaluate(t);
+
+            _currentValue = Mathf.Lerp(_startValue, _targetValue, animationT);
+            UpdateFillAmount();
+
+            yield return null;
+        }
+
+        _currentValue = _targetValue;
+        UpdateFillAmount();
+    }
+
+    private void UpdateFillAmount()
+    {
+        BarImage.fillAmount = Mathf.Clamp01(_currentValue / _maxValue);
     }
 }
