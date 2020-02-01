a6fc2397986e5edca8b2e72de128d156587218cd
Henri Niva <henri.a.niva@gmail.com>
2020-02-01 22:09:33 +0200

    Trex stomp particles

---
+++ src/Assets/TRexAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TRexAnimationController : MonoBehaviour
+{
+    private TRexController _controller;
+
+    void Awake()
+    {
+        _controller = GetComponentInParent<TRexController>();
+    }
+
+    public void StompLeft()
+    {
+        _controller.StompLeft();
+    }
+
+    public void StompRight()
+    {
+        _controller.StompRight();
+    }
+}
